bcff623d115d7043d438099108909e3f8b1a216f
Aakash Goyal <53808525+agoyal1999@users.noreply.github.com>
2020-10-09 23:48:49 +0800

    cfg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import logo from './Assets/Images/HandsOn_favicon.png';
 import Home from './Components/Home/home';
 
 function App() {
 	return (
 		<div>
+			{/* <Header/> */}
+			<img class="ui tiny image" src={logo} />
 			<Home />
 		</div>
 	);
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -42,34 +42,61 @@ class home extends React.Component {
 }
 
 class LogIn extends React.Component {
-	login = () => {
-		return (
-			<form class="ui form">
-				<div class="field">
-					<label>First Name</label>
-					<input type="text" name="first-name" placeholder="First Name" />
-				</div>
-				<div class="field">
-					<label>Last Name</label>
-					<input type="text" name="last-name" placeholder="Last Name" />
-				</div>
-				<div class="field">
-					<div class="ui checkbox">
-						<input type="checkbox" tabindex="0" class="hidden" />
-						<label>I agree to the Terms and Conditions</label>
-					</div>
-				</div>
-				<button class="ui button" type="submit">
-					Submit
-				</button>
-			</form>
-		);
-	};
+	// login = () => {
+	// 	return (
+	// 		<div>
+	// 			<form class="ui form">
+	// 				<div class="field">
+	// 					<label>First Name</label>
+	// 					<input type="text" name="first-name" placeholder="First Name" />
+	// 				</div>
+	// 				<div class="field">
+	// 					<label>Last Name</label>
+	// 					<input type="text" name="last-name" placeholder="Last Name" />
+	// 				</div>
+	// 				<div class="field">
+	// 					<div class="ui checkbox">
+	// 						<input type="checkbox" tabindex="0" class="hidden" />
+	// 						<label>I agree to the Terms and Conditions</label>
+	// 					</div>
+	// 				</div>
+	// 				<button class="ui button" type="submit">
+	// 					Submit
+	// 				</button>
+	// 			</form>
+	// 		</div>
+	// 	);
+	// };
 
 	render() {
 		return (
 			<div>
-				<div />
+				<div id="logInCard" class="ui card" style={{ width: '50%' }}>
+					<form class="ui form">
+						<div class="content">
+							<div class="header">Log In</div>
+						</div>
+						<div class="field">
+							<label>Username</label>
+							<input type="text" name="username" placeholder="Username" />
+						</div>
+						<div class="field">
+							<label>Password</label>
+							<input type="Password" name="password" placeholder="Password" />
+						</div>
+						<button class="ui button" type="submit">
+							Log In
+						</button>
+					</form>
+					<div>
+						<button id="registerBtn" class="small ui button">
+							Register as NGO{' '}
+						</button>
+						<button id="registerBtn" class="small ui button">
+							Register as volunteer{' '}
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
